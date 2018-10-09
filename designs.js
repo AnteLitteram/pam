6de177919c2cd71a382af427b9e39198fbaaf8ef
Auman <mail@auman.me>
2018-10-08 23:19:48 -0400

    add event listener to log click event details

--- designs.js
+++ designs.js
@@ -1,6 +1,6 @@
 // add var to hold id/color clicked/selected
-
-
+let clickId = "50";	//test with static 50 DELETE
+let color = "red";	//test with red DELETE
 
 // Select color input
 // get value from #colorPicker
@@ -13,6 +13,7 @@
 function getWidth () {
 	var params = new URLSearchParams(document.location.search.substring(1));
 	var width = params.get("width");
+
 	return width;
 };
 
@@ -26,15 +27,19 @@ function getHeight () {
 //need function to get id= from square that click occurs in
 //
 
+
+
 //need function to change background-color for <div> with id num clicked
-function addColor(id) {
-document.getElementById(id).style.backgroundColor = 'color';
-}
+
+
 
 
 // When size is submitted by the user, call makeGrid()
 //may need to add when DOM is ready code
 document.addEventListener('submit', makeGrid () );
+document.addEventListener('click', function (event) {
+	console.log(event);
+});
 
 
 
